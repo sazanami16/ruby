72b8521428c69a23da3b1d51f7ab90fe22af5526
sazanami16 <za_nami.za_nami16@icloud.com>
2021-04-30 15:01:59 +0900

    キーと値も使い方

--- ruby.rb
+++ ruby.rb
@@ -1,9 +1,9 @@
-fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
-
-fruits_price.each do |fruits|
-  num = 0 
-  fruits[1].each do |price|
-    num += price
-  end
-  puts "#{fruits[0]}の合計金額は#{num}円です"
+fruits = { "Apple" => "150円", "Orange" => "100円", "Melon" => "600円", "Grape" => "700円" }
+fruits["lemon"] = "500円"
+fruits["lemon"] = "800円"
+ 
+fruits.each do |key_value|
+key = key_value[0]
+value = key_value[1]
+puts "#{key}は、#{value}"
 end
