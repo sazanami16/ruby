64eeb95ac8687aea913c646838a9e9b81a44847d
sazanami16 <za_nami.za_nami16@icloud.com>
2021-05-02 22:18:39 +0900

    変数複数定義

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,21 @@
-i = 0
-
-while i <= 64
-  i += 1
-  num = i ** 4
-  puts num
+def addition(a, b)
+  a + b
 end
+
+def multiplication(a,b)
+  a * b
+end
+
+def slice_num(num)
+  t = (num / 10) % 10
+  o = num % 10
+  return t, o
+end
+
+puts "二桁の整数を入力してください"
+i = gets.to_i
+
+X, Y = slice_num(i)
+add_result = addition(X, Y)
+multiple_result = multiplication(X, Y)
+puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
