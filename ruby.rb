d6e5c7da610007fe69904404152efeed3470fc4d
sazanami16 <za_nami.za_nami16@icloud.com>
2021-05-02 22:30:02 +0900

    sliceメソッド

--- ruby.rb
+++ ruby.rb
@@ -1,21 +1,8 @@
-def addition(a, b)
-  a + b
+def slice_str(str)
+  r = str.slice(-2, 2)
+  puts r * 3
 end
 
-def multiplication(a,b)
-  a * b
-end
-
-def slice_num(num)
-  t = (num / 10) % 10
-  o = num % 10
-  return t, o
-end
-
-puts "二桁の整数を入力してください"
-i = gets.to_i
-
-X, Y = slice_num(i)
-add_result = addition(X, Y)
-multiple_result = multiplication(X, Y)
-puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
+puts "文字を入力してください"
+s = gets.chomp
+slice_str(s)
