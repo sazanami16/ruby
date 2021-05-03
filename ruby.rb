f7e9bf7601d2cd7ba7379592ad1c8be50ce63461
sazanami16 <za_nami.za_nami16@icloud.com>
2021-05-03 20:01:41 +0900

    文字検索アルゴリズム

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,28 @@
-def slice_str(str)
-  r = str.slice(-2, 2)
-  puts r * 3
+def binary_search(array, right, target)
+  left = 0
+  while left <= right
+    center = (left + right) / 2
+    if array[center] == target
+      return center
+    elsif array[center] < target
+      left = center + 1
+    else
+      right = center - 1
+    end
+  end
+  return -1 
 end
 
-puts "文字を入力してください"
-s = gets.chomp
-slice_str(s)
+array=[1,3,5,6,9,10,13,20,26,31]
+
+puts "検索したい数字を入力してください"
+target = gets.to_i
+number_of_elements = array.count
+
+result = binary_search(array, number_of_elements, target)
+
+if result == -1
+  puts "#{target}は配列内に存在しません"
+else
+  puts "#{target}は配列の#{result + 1}番目に存在します "
+end
