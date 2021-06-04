1405641ea9bef57fc76d847ca4edc028bc75db1a
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-04 16:47:56 +0900

    split

--- ruby.rb
+++ ruby.rb
@@ -1,11 +1,4 @@
-loop = gets.chomp.to_i # 文字を入力する回数を任意で入力できるようにして、その回数分入力した文字が出力される。
+str = "app leomon banana".split(' ') # ()内で分割する文字または空白値、何分割するかを指定できる。
+puts str
 
-(1..loop).each do |i|
-    output = gets.chomp
-    puts output
-end
-
-
-
-num = gets.chomp.to_i  # 簡略化した記述方法
-(0...num).each { puts gets.chomp }
+# splitはStringクラスのメソッドなので、String以外に使用するとエラーになる。
