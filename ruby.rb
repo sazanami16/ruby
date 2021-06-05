a6a1378709cd8ac4d1ffedaf38c38423e839d9ad
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-05 21:28:51 +0900

    イニシャルに変換

--- ruby.rb
+++ ruby.rb
@@ -1,16 +1,2 @@
-num = gets.chomp.to_i
-
-(1..num).each do |i|
-    str = gets.chomp
-    
-    case str 
-    when 'forward' then
-        puts 'Sunny'
-    when 'reverse'
-        puts 'Rainy'
-    when 'sideways'
-        puts 'Cloudy'
-    else
-        'false'
-    end
-end
+str = gets.chomp
+puts str.split.map{|s| s[0]}.join('.') + '.'
