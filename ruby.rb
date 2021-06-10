dbb238bb3917bc96bdff276da3742327c2d3db44
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-11 00:24:45 +0900

    五目ならべ　その３

--- ruby.rb
+++ ruby.rb
@@ -3,17 +3,34 @@ result = 'D'
 
 (1..5).each { array.push(gets.chomp.split(''))} #配列の中に文字を5回入力して、それを配列化する処理。
   
-  (0..4).each do |i| #配列の中の文字列を順番に検証する処理。
+  (1..2).each do |time| 
     o = 0
     x = 0
-    array.each do |row| # 1つの配列のなかの文字を順番に検証していく処理。
-      if row[i] == 'O' #[[0],[1],[2],[3],[4]]の「0[0]から4[0]」のセットで検証していく処理。全ての配列が'O'か'X'の場合は、１を足していき、５になった場合は、下記のif文の通り結果を返す。
-        o = o + 1
-      elsif row[i] == 'X'
-        x = x + 1
-      end
+    i = 0
+    if time == 1
+      j = 0
+    else
+      j = 4
     end
 
+    (1..5).each do
+      if array[i][j] == 'O'
+        o = o + 1
+      elsif board[i][j] == 'X'
+        x = x + 1
+      else
+        break
+      end
+  
+      i = i + 1
+  
+      if time == 1
+        j = j + 1
+      else
+        j = j - 1
+      end
+    end
+  
     if o == 5
       result = 'O'
       break
@@ -24,4 +41,3 @@ result = 'D'
   end
   
   puts result
-
