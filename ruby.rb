7befdf1ef0e3a7b2eb56480945af52777de0bf6a
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-10 23:49:38 +0900

    五目並べ　その２

--- ruby.rb
+++ ruby.rb
@@ -1,16 +1,27 @@
-array = %w[O X]
+array = [] #[[0],[1],[2],[3],[4]]という形の配列で文字が入るようになっている。
 result = 'D'
 
-(1..5).each do #5文字の入力を5回できるようにしている。
-  string = gets.chomp.split('') #入力した文字を配列化している。
+(1..5).each { array.push(gets.chomp.split(''))} #配列の中に文字を5回入力して、それを配列化する処理。
   
-  array.each do |a| #「%w[O X]」をそれぞれ検証している。
-    cnt = 0 #配列のインデックスの初期値
-    string.each { |s| cnt = cnt + 1 if s == a } #配列化させた文字がarrayの文字と一致していれば、次の文字を検証している。
-     if cnt >= 5 #上記の検証が5回に到達すれば、resultは「 O 」か「 X 」になる。到達しなければ、「 D 」となる
-      result = a
-     end 
+  (0..4).each do |i| #配列の中の文字列を順番に検証する処理。
+    o = 0
+    x = 0
+    array.each do |row| # 1つの配列のなかの文字を順番に検証していく処理。
+      if row[i] == 'O' #[[0],[1],[2],[3],[4]]の「0[0]から4[0]」のセットで検証していく処理。全ての配列が'O'か'X'の場合は、１を足していき、５になった場合は、下記のif文の通り結果を返す。
+        o = o + 1
+      elsif row[i] == 'X'
+        x = x + 1
+      end
+    end
+
+    if o == 5
+      result = 'O'
+      break
+    elsif x == 5
+      result = 'X'
+      break
+    end
   end
-end
-  puts result
   
+  puts result
+
