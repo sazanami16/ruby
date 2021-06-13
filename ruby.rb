26e3f49dbcd419fd90e8ccdcca1c925e0685be1b
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-13 21:36:01 +0900

    2つのリストの重複を一意にしたリスト

--- ruby.rb
+++ ruby.rb
@@ -1,26 +1,6 @@
-def find_roots(a, b, c)
-  ax ^ 2 + bx + c = 0
+def unique_names(names1, names2)
+  names1.concat(names2)
+  p names1.uniq
 end
 
-def find_roots_sol1(a, b, c)
-  find_roots = find_roots(a, b, c)
-  if find_roots >= 0
-    
-  end
-end
-
-def find_roots_sol2(a, b, c)
-  find_roots = find_roots(a, b, c)
-  if find_roots >= 0
-    
-  end
-
-def find_roots_eq(a, b, c)
-  find_roots = find_roots(a, b, c)
-  if find_roots > 0
-    [find_roots_sol1(a, b, c), find_roots_sol2(a, b, c)]
-  elsif find_roots = 0
-  end
-end
-
-find_roots(2, 10, 8)
+unique_names(['Ava', 'Emma', 'Olivia'], ['Olivia', 'Sophia', 'Emma'])
