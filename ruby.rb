d432b955fe05d8d46c5bbb725176b085d24bed14
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-13 22:14:55 +0900

    トッピングの全ての組み合わせを表示させる(改)

--- ruby.rb
+++ ruby.rb
@@ -3,6 +3,7 @@ class IceCreamMachine
 
     def scoops(items1, items2)
       p items1.product(items2).map{|e| e.join()}
+      p items1.map{|elem_a| items2.map{|elem_b| elem_a.to_s + elem_b.to_s}}.flatten
     end
 end
 
