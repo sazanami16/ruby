c2c145940c0cbb8896789f278a3271654166ffdb
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-13 21:04:00 +0900

    問題

--- ruby.rb
+++ ruby.rb
@@ -1,43 +1,26 @@
-array = [] #[[0],[1],[2],[3],[4]]という形の配列で文字が入るようになっている。
-result = 'D'
+def find_roots(a, b, c)
+  ax ^ 2 + bx + c = 0
+end
 
-(1..5).each { array.push(gets.chomp.split(''))} #配列の中に文字を5回入力して、それを配列化する処理。
-  
-  (1..2).each do |time| 
-    o = 0
-    x = 0
-    i = 0
-    if time == 1
-      j = 0
-    else
-      j = 4
-    end
-
-    (1..5).each do
-      if array[i][j] == 'O'
-        o = o + 1
-      elsif board[i][j] == 'X'
-        x = x + 1
-      else
-        break
-      end
-  
-      i = i + 1
-  
-      if time == 1
-        j = j + 1
-      else
-        j = j - 1
-      end
-    end
-  
-    if o == 5
-      result = 'O'
-      break
-    elsif x == 5
-      result = 'X'
-      break
-    end
+def find_roots_sol1(a, b, c)
+  find_roots = find_roots(a, b, c)
+  if find_roots >= 0
+    
   end
-  
-  puts result
+end
+
+def find_roots_sol2(a, b, c)
+  find_roots = find_roots(a, b, c)
+  if find_roots >= 0
+    
+  end
+
+def find_roots_eq(a, b, c)
+  find_roots = find_roots(a, b, c)
+  if find_roots > 0
+    [find_roots_sol1(a, b, c), find_roots_sol2(a, b, c)]
+  elsif find_roots = 0
+  end
+end
+
+find_roots(2, 10, 8)
